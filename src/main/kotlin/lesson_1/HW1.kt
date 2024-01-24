516a36e2634d78e49abd3fbeeb162b82157339d7
Brightvl <ziro-x-100@mail.ru>
2024-01-24 16:04:16 +0300

    HW1 -> Done

--- src/main/kotlin/lesson_1/HW1.kt
+++ src/main/kotlin/lesson_1/HW1.kt
@@ -1,6 +1,5 @@
 package lesson_1
 
-
 /**
  * Написать программу, которая обрабатывает введённые пользователем в консоль команды:
  * exit
@@ -11,29 +10,68 @@ package lesson_1
  * После выполнения команды, кроме команды exit, программа ждёт следующую команду.
  *
  * Имя – любое слово.
- * Если введена команда с номером телефона, нужно проверить, что указанный телефон может начинаться с +, затем идут только цифры. При соответствии введённого номера этому условию – выводим его на экран вместе с именем, используя строковый шаблон. В противном случае - выводим сообщение об ошибке.
- * Для команды с электронной почтой делаем то же самое, но с другим шаблоном – для простоты, адрес должен содержать три последовательности букв, разделённых символами @ и точкой.
+ * Если введена команда с номером телефона, нужно проверить, что указанный телефон может начинаться с +, затем идут только цифры.
+ * При соответствии введённого номера этому условию – выводим его на экран вместе с именем, используя строковый шаблон.
+ * В противном случае - выводим сообщение об ошибке.
+ * Для команды с электронной почтой делаем то же самое, но с другим шаблоном – для простоты, адрес должен содержать три
+ * последовательности букв, разделённых символами @ и точкой.
  *
  * Пример команд:
  * add Tom email tom@example.com
  * add Tom phone +7876743558
  */
 
-fun main() {
-    var text = input()
+// хранилище контактов
+val contacts = HashMap<String, MutableMap<String, String>>()
 
-    while (text != "exit") {
-        text = input()
+/**
+ * Для правильной разбивки и добавления в словарь contacts
+ */
+private fun addContact(name: String, contactType: String, value: String) {
+    if (contactType == "phone") {
+        if (Regex("""^\+\d+$""").matches(value)) { // строка начинается с + и ы строке одни цифры
+            contacts[name] = mutableMapOf("phone" to value) // phone - ключ
+            println("Added phone number for $name: $value")
+        } else {
+            println("Error: Invalid phone number format")
+        }
+    } else if (contactType == "email") {
+        if (Regex("""^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$""").matches(value)) { //  строка по email
+            contacts[name] = mutableMapOf("email" to value)
+            println("Added email for $name: $value")
+        } else {
+            println("Error: Invalid email format")
+        }
     }
 }
 
-fun input(): String? {
-    println("Введите текст")
-    return readlnOrNull()
+fun main() {
+    runProgram()
 }
 
-fun choice(input: String) {
-    when {
+private fun runProgram() {
+    while (true) {
+        println("Введите команду ")
+        when (val command = readlnOrNull()) { //switch
+            "exit" -> break
+            "help" -> println(
+                "Доступные команды: " +
+                        "exit, " +
+                        "help, " +
+                        "add <Имя> phone <Номер телефона>, " +
+                        "add <Имя> email <Адрес электронной почты>"
+            )
 
+            else -> { // как default в java
+                val parts = command?.split(" ") // разбиваем
+                if (parts?.size == 4 && parts[2] == "phone") {
+                    addContact(parts[1], "phone", parts[3])
+                } else if (parts?.size == 4 && parts[2] == "email") {
+                    addContact(parts[1], "email", parts[3])
+                } else {
+                    println("Error: Invalid command format")
+                }
+            }
+        }
     }
 }

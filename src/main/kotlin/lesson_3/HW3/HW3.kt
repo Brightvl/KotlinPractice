0a2f5b134963bf3d2c64af62ab239264d957c7ec
Iaroslav S <ziro-x-100@mail.ru>
2024-02-01 10:38:28 +0300

    HW3 -> Done

--- src/main/kotlin/lesson_3/HW3/HW3.kt
+++ src/main/kotlin/lesson_3/HW3/HW3.kt
@@ -1,5 +1,8 @@
 package lesson_3.HW3
 
+import kotlin.system.exitProcess
+
+
 /**
  * — Измените класс Person так, чтобы он содержал список телефонов и список почтовых адресов, связанных с человеком.
  * — Теперь в телефонной книге могут храниться записи о нескольких людях. Используйте для этого наиболее подходящую структуру данных.
@@ -8,113 +11,130 @@ package lesson_3.HW3
  * — Команда show должна принимать в качестве аргумента имя человека и выводить связанные с ним телефоны и адреса электронной почты.
  * — Добавьте команду find, которая принимает email или телефон и выводит список людей, для которых записано такое значение.
  */
-//region sealed class
-sealed class Command {
-    abstract fun isValid(): Boolean
+
+sealed interface Command {
+    fun isValid(): Boolean
+    fun execute()
 }
 
-data class AddPhone(val name: String, val phone: String) : Command() {
+data class AddPhone(val name: String, val phone: String) : Command {
     override fun isValid() = Regex("""^\+\d+$""").matches(phone)
+
+    override fun execute() {
+        val person = contacts.getOrPut(name) { Person(name) }
+        person.phones.add(phone)
+        println("Контакт $person добавлен")
+    }
 }
 
-data class AddEmail(val name: String, val email: String) : Command() {
-    override fun isValid() = Regex("""^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$""").matches(email)
+data class AddEmail(val name: String, val email: String) : Command {
+    override fun isValid() = Regex("""^[a-zA-Zа-яА-Я0-9]+@[a-zA-Z]+\.[a-zA-Z]+$""").matches(email)
+
+    override fun execute() {
+        val person = contacts.getOrPut(name) { Person(name) }
+        person.emails.add(email)
+        println("Контакт $person добавлен")
+    }
 }
 
-data class Show(val name: String) : Command() {
+data object Help : Command {
     override fun isValid() = true
+
+    override fun execute() {
+        val commands =
+            listOf(
+                "exit",
+                "help",
+                "add <Имя> phone <Номер телефона>",
+                "add <Имя> email <Адрес электронной почты>",
+                "show <Имя>",
+                "find <Телефон или Email>"
+            )
+        println("Доступные команды: ${commands.joinToString(", ")}")
+    }
 }
 
-data class Find(val contact: String) : Command() {
+data object Exit : Command {
     override fun isValid() = true
+
+    override fun execute() {
+        exitProcess(0)
+    }
 }
 
-data object Help : Command() {
+data class Show(val name: String?) : Command {
     override fun isValid() = true
-}
 
-data object Exit : Command() {
-    override fun isValid() = true
-}
-//endregion
-
-data class Person(var name: String, var phones: MutableList<String> = mutableListOf(), var emails: MutableList<String> = mutableListOf())
-
-fun main() {
-    val contacts = mutableMapOf<String, Person>()
-    var lastPerson: Person? = null
-
-    while (true) {
-        try {
-            val command = readCommand()
-            if (!command.isValid()) {
-                showCommands()
-                continue
-            }
-
-            when (command) {
-                is Exit -> break
-                is Help -> showCommands()
-                is AddPhone -> {
-                    val person = contacts.getOrPut(command.name) { Person(command.name) }
-                    person.phones.add(command.phone)
-                    lastPerson = person
-                    println("Добавлен телефон для ${person.name}: Телефоны=${person.phones.joinToString(", ")}")
-                }
-                is AddEmail -> {
-                    val person = contacts.getOrPut(command.name) { Person(command.name) }
-                    person.emails.add(command.email)
-                    lastPerson = person
-                    println("Добавлен email для ${person.name}: Emails=${person.emails.joinToString(", ")}")
-                }
-                is Show -> {
-                    val person = contacts[command.name]
-                    if (person != null) {
-                        println("Контакт для ${person.name}: Телефоны=${person.phones.joinToString(", ")}, Emails=${person.emails.joinToString(", ")}")
-                    } else {
-                        println("Контакт не найден")
-                    }
-                }
-                is Find -> {
-                    val foundPersons = contacts.values.filter { it.phones.contains(command.contact) || it.emails.contains(command.contact) }
-                    if (foundPersons.isNotEmpty()) {
-                        println("Найденные контакты:")
-                        foundPersons.forEach { person ->
-                            println("${person.name}: Телефоны=${person.phones.joinToString(", ")}, Emails=${person.emails.joinToString(", ")}")
-                        }
-                    } else {
-                        println("Контакт не найден")
-                    }
-                }
-            }
-        } catch (e: IllegalArgumentException) {
-            println("Ошибка: ${e.message}")
+    override fun execute() {
+        val person = contacts[name]
+        if (person != null) {
+            println("${person.name}'s phones: ${person.phones.joinToString(", ")}")
+            println("${person.name}'s emails: ${person.emails.joinToString(", ")}")
+        } else {
+            println("Контакт не найден.")
         }
     }
-
-    lastPerson?.let { println("Последний добавленный человек: $it") } ?: println("Не инициализирован")
 }
 
+data class Find(val value: String?) : Command {
+    override fun isValid() = true
+
+    override fun execute() {
+        val matchingContacts = contacts.filter {
+            it.value.phones.contains(value) || it.value.emails.contains(value) }
+        if (matchingContacts.isNotEmpty()) {
+            println("Найденные контакты:")
+            for ((name, person) in matchingContacts) {
+                println("$name - Phones: ${
+                    person.phones.joinToString(", ")} - Emails: " +
+                        person.emails.joinToString(", ")
+                )
+            }
+        } else {
+            println("Контакты не найдены.")
+        }
+    }
+}
+
+data class Person(
+    var name: String,
+    var phones: MutableList<String> = mutableListOf(),
+    var emails: MutableList<String> = mutableListOf()
+)
+
+val contacts = mutableMapOf<String, Person>()
+
 fun readCommand(): Command {
-    val input = readln().split(" ")
+    print("Введите команду: ")
+    val input = readLine()?.split(" ") ?: throw IllegalArgumentException("Неверная команда")
     return when (input[0]) {
         "exit" -> Exit
         "help" -> Help
         "add" -> {
             val name = input[1]
             when (input[2]) {
-                "phone" -> AddPhone(name, input[3])
-                "email" -> AddEmail(name, input[3])
+                "phone" -> AddPhone(name, phone = input[3])
+                "email" -> AddEmail(name, email = input[3])
                 else -> throw IllegalArgumentException("Неверная команда")
             }
         }
-        "show" -> Show(input[1])
-        "find" -> Find(input[1])
+
+        "show" -> Show(input.getOrNull(1))
+        "find" -> Find(input.getOrNull(1))
         else -> throw IllegalArgumentException("Неверная команда")
     }
 }
 
-fun showCommands() {
-    val commands = listOf("exit", "help", "add <Имя> phone <Номер телефона>", "add <Имя> email <Адрес электронной почты>", "show <Имя>", "find <Телефон или Email>")
-    println("Доступные команды: ${commands.joinToString(", ")}")
+fun main() {
+    while (true) {
+        try {
+            val command = readCommand()
+            if (command.isValid()) {
+                command.execute()
+            }
+        } catch (e: IllegalArgumentException) {
+            println("Ошибка: ${e.message}")
+        }
+    }
 }
+
